bd8584affe19b64b1fb400661d589e6d7944f140
vinaysandesh <vinaysandesh35@gmail.com>
2021-08-29 01:02:42 +0530

    removed next js

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
-import { Animated, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ABOUT, WORK, BLOG, CONTACT, HOME } from './Constant/TextConstants';
-import '@expo/match-media'
-// Unleash the demo :D
-import { useMediaQuery } from "react-responsive";
-import Typed from 'react-typed';
-import AboutLeft from './Components/AboutLeft';
-import AboutRight from './Components/AboutRight';
-import Follow from './Components/Header/Follow';
-import WorkMain from './Components/Work/WorkMain';
-import BlogMain from './Components/Blog/BlogMain';
-import MainScreen from './Components/MainStack/MainScreen'
+import {  StyleSheet, Text, TouchableOpacity, View } from 'react-native'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Tools from './Components/Screens/Tools';
-import Contact from './Components/Contact/Contact'; 
+import Tools from './Components/Screens/Tools'; 
 import Index from './Components/MainStack/Index';
 import BlogIndex from './Blog/BlogIndex';
 import { ApolloProvider } from '@apollo/client';
@@ -25,27 +13,8 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 const Stack = createStackNavigator();
-export default function App() {
-  const inputEl = useRef(null);
-  const [bodyHeight,setBodyHeight] = useState(0)
-  const [selectedNavitem,setSelectednavItem] = useState("about")
+export default function App() { 
  
-  // useEffect(()=>{
-  //   if(inputEl.current){
-  //     inputEl.current.measure((fx, fy, width, height, px, py) => {
-  //       setBodyHeight(height-0.5)
-  //     });
-      
-  //   }
-  // },[inputEl])
-  // const isTabletOrMobileDevice = useMediaQuery({    
-  //   maxDeviceWidth: 1224,
-  //   // alternatively...
-  //   query: "(max-device-width: 1224px)"  
-  // });
-  // if (isTabletOrMobileDevice) {
-  //   return (<Text>Hi Mobile Users 👋</Text>)
-  // }
   const config = {
     screens: {
       MainScreen: '',
@@ -100,9 +69,7 @@ export default function App() {
        
         <Stack.Screen name="MainScreen" component={Index} options={{title:"Home"}}/>
         <Stack.Screen name="BlogIndex" component={BlogIndex}/>
-        <Stack.Screen name="BlogPost" component = {BlogPost}/>
-        <Stack.Screen name="Tools" component={Tools}/>
-        <Stack.Screen name={"NotFound"} component={Contact}/> 
+        <Stack.Screen name="BlogPost" component = {BlogPost}/> 
       </Stack.Navigator>
     </NavigationContainer>
     </PaperProvider>
